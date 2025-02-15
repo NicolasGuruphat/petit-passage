d10bbdda08afa52ada4fba4212731a914d8ad0a7
GregoryMoutote <gregory.moutote@etu.univ-lyon1.fr>
2025-02-15 18:37:46 +0100

    disquettributeur animation wip

---
+++ .vercel/output/static/js/disquettributeur.js
@@ -0,0 +1,110 @@
+const draw1button = document.getElementById("draw1button");
+const draw10button = document.getElementById("draw10button");
+const machine = document.getElementById("disquettributeur");
+const machineHandle = document.getElementById("gachapon_handle");
+// const gachaball = document.getElementById("gachaball");
+
+const smoke_explosion = document.createElement("img");
+smoke_explosion.src = "img/smoke_explosion_once.gif";
+smoke_explosion.id = "smoke-explosion";
+
+const spotlight = document.createElement("div");
+spotlight.id = "spotlight";
+spotlight.style.background = `radial-gradient(circle at ${window.innerWidth / 2}px ${window.innerHeight / 2}px, #00000000 10px, #000000ee  ${window.innerHeight / 2}px)`;
+
+const lootedText = document.createElement("p");
+lootedText.id = "looted-text";
+lootedText.textContent = "Wait there is still nothing here !"
+
+const gachaball = document.createElement("img");
+gachaball.src = "img/gachaball_cb.svg";
+gachaball.id = "gachaball";
+
+const claimButton = document.createElement("button");
+claimButton.textContent = "Récupérer";
+claimButton.id = "claim-button";
+claimButton.addEventListener('click', () => {
+  delElement(lootedText);
+  delElement(spotlight);
+  delElement(claimButton);
+});
+
+const gachaponShaking = [
+  { transform: "rotate(0deg)" },
+  { transform: "rotate(5deg)" },
+  { transform: "rotate(0deg)" },
+  { transform: "rotate(-5deg)" },
+  { transform: "rotate(0deg)" },
+];
+
+const handleRotating = [
+  { transform: "rotate(0deg)" },
+  { transform: "rotate(-180deg)" },
+  { transform: "rotate(0deg)" },
+];
+
+const ballFalling = [
+  // { transform: "rotate(90deg)" },
+  // { display: "block" },
+  // { transform: "translateY(-15%)" },
+  // { transform: "translateY(-15%)" },
+  // { transform: "translateY(-15%)" },
+  // { transform: "translateY(-15%)" },
+  // { transform: "translateY(-15%)" },
+  // { transform: "translateY(-15%)" },
+  { transform: "translateY(18%) ease-out" },
+  { transform: "translateY(-18%) ease-in" },
+];
+
+  
+const gachaponShakingTiming = {
+  duration: 100,
+  iterations: 10,
+  delay: 750,
+};
+
+const handleRotatingTiming = {
+  duration: 750,
+  iterations: 1,
+};
+
+  
+const ballFallingTiming = {
+  duration: 4000,
+  delay: 1750,
+};
+
+function delElement(element) {
+  element.remove()
+}
+
+function addClaimButton() {
+  machine.appendChild(claimButton);
+}
+
+function spawnSmoke() {
+  machine.appendChild(smoke_explosion);
+
+  setTimeout(function() { delElement(smoke_explosion); }, 800);
+  
+  machine.appendChild(spotlight);
+  
+  machine.appendChild(lootedText);
+
+  setTimeout(function() { addClaimButton(); }, 2000);
+}
+
+
+draw1button.addEventListener('click', () => {
+  machineHandle.animate(handleRotating, handleRotatingTiming);
+  machine.animate(gachaponShaking, gachaponShakingTiming);
+  machine.appendChild(gachaball);
+  setTimeout(function() { spawnSmoke(); }, 2650);
+});
+
+draw10button.addEventListener('click', () => {
+  machineHandle.animate(handleRotating, handleRotatingTiming);
+  machine.animate(gachaponShaking, gachaponShakingTiming);
+  machine.appendChild(gachaball);
+  setTimeout(function() { spawnSmoke(); }, 2650);
+});
